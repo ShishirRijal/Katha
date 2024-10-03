1fca876b0d514e17e5e7090d47bc939daa4c23fb
Shishir Rijal <ccrrizal0892@gmail.com>
2024-10-04 03:33:11 +0545

    add color extension

---
+++ Katha/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Katha
+//
+//  Created by Shishir Rijal on 04/10/2024.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let primary = Color("PrimaryColor")
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+}
