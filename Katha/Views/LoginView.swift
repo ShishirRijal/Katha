093e57f445e610a38b18de6753073a9e35a0aef8
Shishir Rijal <ccrrizal0892@gmail.com>
2024-10-04 14:58:59 +0545

    add login view

---
+++ Katha/Views/LoginView.swift
@@ -0,0 +1,76 @@
+//
+//  LoginView.swift
+//  Katha
+//
+//  Created by Shishir Rijal on 04/10/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var email: String = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                
+                BackButton()
+                
+                TitleView()
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                VStack(alignment: .leading) {
+                    Text("Your email")
+                        
+                    CustomTextField(placeholder: "rijal.shishir@test.com", text: $email)
+                }
+                
+                Spacer()
+                    .frame(height: 30)
+                
+                CustomButton(title: "Continue") {
+                    //
+                }
+                
+                Spacer()
+                
+            }
+            .padding()
+        }
+    }
+    
+    
+    // MARK: BackButton
+    private struct BackButton: View {
+        var body: some View {
+            HStack {
+                NavigationLink(destination: AuthView()) {
+                    Image(systemName: "chevron.left") // Back icon
+                        .font(.title) // Adjust the size of the icon
+                        .foregroundColor(.black) // Set the icon color to black
+                }
+                Spacer() // Spacer to push the button to the left
+            }
+        }
+    }
+    
+    //MARK: Title View
+    private struct TitleView: View {
+        var body: some View {
+            VStack {
+                Text("Medium")
+                    .font(.logoFont())
+                Spacer().frame(height: 20) // Space between the title and the next text
+                Text("Sign in with email.")
+                    .font(.custom(.playfairDisplayMedium, size: 30))
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    LoginView()
+}
