34de3a4f3d2d1835234b441c6ccd3955b41f516a
Shishir Rijal <ccrrizal0892@gmail.com>
2024-10-14 01:53:24 +0545

    add explore view

--- Katha/Views/ExploreView.swift
+++ Katha/Views/ExploreView.swift
@@ -8,19 +8,154 @@
 import SwiftUI
 
 struct ExploreView: View {
+    
+    @State private var searchText = ""
+    
+    
+    var body: some View {
+        ScrollView {
+            VStack (alignment: .leading) {
+                
+                Header()
+                SearchBarView(searchText: $searchText)
+            
+                PopularSearch()
+                    .padding(.vertical, 20)
+                
+                Divider()
+                    .frame(height: 2)
+                    .background(Color(.systemGray4))
+                    .padding(.bottom, 20)
+                    
+                
+                HStack {
+                    Image(systemName: "chart.line.uptrend.xyaxis.circle")
+                        .font(.title)
+                    
+                    Text("Trending on medium")
+                        .textCase(.uppercase)
+                        .font(.custom(.poppinsMedium, size: 16))
+                    
+                }
+                .foregroundColor(.primary)
+                
+                
+                ForEach(Array(dummyArticles.enumerated()), id: \.element.id) { index, article in
+                    TrendingItem(index: index + 1, article: article)
+                        .padding(.bottom, 10)
+                }
+                    
+                Spacer()
+                
+            }
+            .padding()
+        }
+    }
+    
+    struct Header: View {
+        var body: some View {
+            HStack() {
+                Text("Explore")
+                    .font(.custom(.poppinsMedium, size: 30))
+                    .fontWeight(.bold)
+                
+                Spacer()
+            }
+        }
+    }
+        
+        
+}
+
+
+struct SearchBarView: View {
+    @Binding var searchText: String
+
     var body: some View {
         HStack {
-            Text("Explore")
-                .font(.logoFont())
-            .fontWeight(.bold)
-            
+            // Search icon
             Image(systemName: "magnifyingglass")
-                .font(.title)
-                .foregroundColor(.theme.accent)
+                .foregroundColor(.gray)
+            
+            // Search field with placeholder
+            TextField("Search Medium...", text: $searchText)
+            
+        }
+        .padding()
+        .background(Color(.systemGray5))
+        .cornerRadius(5)
+    }
+}
+
+
+
+#Preview {
+    ExploreView()
+}
+
+struct PopularSearch: View {
+    var topSearches = ["UX", "iOS Development", "Gaming", "Apple", "Flutter"]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(topSearches, id: \.self) {item in
+                    Text(item)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 20)
+                        .background(Color(.systemGray5))
+                        .foregroundColor(.primary)
+                        .cornerRadius(30)
+                }
+            }
         }
     }
 }
 
-#Preview {
-    ExploreView()
+
+struct TrendingItem: View {
+    var index: Int
+    var article: Article
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            VStack(alignment: .leading) {
+                HStack(alignment: .top) {
+                    
+                    Text(String(format: "%02d", index))
+                        .font(.custom("Poppins-Bold", size: 40))
+                        .foregroundColor(Color(.systemGray))
+                        .padding(.trailing, 10)
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(article.author.image)
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .clipShape(Circle())
+                                .padding(.trailing, 5)
+                            
+                            Text(article.author.name)
+                                .font(.custom("Poppins-Medium", size: 18))
+                            
+                            Spacer()
+                        }
+                        .padding(.top, 10)
+                        
+                        Text(article.title)
+                            .font(.custom("Poppins-Bold", size: 18))
+                            .lineLimit(2)
+                            .foregroundColor(.primary)
+                        
+                        HStack {
+                            Text("7 min read") // You can make this dynamic if available
+                            Text("•")
+                            Text("\(article.date.timeAgoDisplay())")
+                        }
+                        .foregroundColor(.primary)
+                    }
+                }
+            }
+        }
+    }
 }
